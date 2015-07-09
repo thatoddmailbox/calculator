d6544c26c9792d8824585ecb1d670127f1c0a468
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-07-09 17:45:44 +0000

    move main menu

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,4 @@
-
-
+require_relative "./mainmenu"
 
 def division(num1,num2)
   return "#{num1/num2}" 
@@ -33,55 +32,6 @@ def addition(numbers)
     sum = sum + number # add the number to the total
   end
   return sum # return the total value
-end
-
-def main_menu()
-  puts "Calculator"
-  puts
-  
-  puts "Select operation or type 'quit' to quit: "
-  operation = gets.chomp
-  
-  if operation == "quit"
-    # quit
-    return false
-  end
-  
-  if operation == "%"
-    percent
-    return true
-  end
-  
-  asking_for_numbers = true
-  numbers = []
-  
-  while asking_for_numbers do
-    # keep asking for a number
-    puts "Enter number or '=': "
-    number = gets.chomp
-    if number == "="
-      # do calculation
-      result = 0
-      if operation == "+"
-        result = addition(numbers)
-      elsif operation == "-"
-        result = subtraction(numbers)        
-      elsif operation == "x"
-        result = multiply(numbers)        
-      elsif operation == "/"
-        result = division(numbers)        
-      elsif operation == "^"
-        result = power(numbers)
-      else
-        result = "Unknown operation"
-      end
-      puts "Reuslt: #{result}"
-      break
-    else
-      numbers << number.to_i # add number to array
-    end
-  end
-  return true
 end
 
 while true do
---
+++ mainmenu.rb
@@ -0,0 +1,48 @@
+def main_menu()
+  puts "Calculator"
+  puts
+  
+  puts "Select operation or type 'quit' to quit: "
+  operation = gets.chomp
+  
+  if operation == "quit"
+    # quit
+    return false
+  end
+  
+  if operation == "%"
+    percent
+    return true
+  end
+  
+  asking_for_numbers = true
+  numbers = []
+  
+  while asking_for_numbers do
+    # keep asking for a number
+    puts "Enter number or '=': "
+    number = gets.chomp
+    if number == "="
+      # do calculation
+      result = 0
+      if operation == "+"
+        result = addition(numbers)
+      elsif operation == "-"
+        result = subtraction(numbers)        
+      elsif operation == "x"
+        result = multiply(numbers)        
+      elsif operation == "/"
+        result = division(numbers)        
+      elsif operation == "^"
+        result = power(numbers)
+      else
+        result = "Unknown operation"
+      end
+      puts "Reuslt: #{result}"
+      break
+    else
+      numbers << number.to_i # add number to array
+    end
+  end
+  return true
+end
